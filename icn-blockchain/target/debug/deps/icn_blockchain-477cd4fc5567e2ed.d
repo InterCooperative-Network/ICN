bc01288764e5b22039f5a2cb22bfaca94b57c7ae
fahertym <matt.faherty@gmail.com>
2024-10-31 00:06:23 +0000

    Added transaction and reputation modules: Transaction module enables pseudonymous exchanges with secure hashing, and ReputationSystem tracks cooperative contributions.

--- icn-blockchain/target/debug/deps/icn_blockchain-477cd4fc5567e2ed.d
+++ icn-blockchain/target/debug/deps/icn_blockchain-477cd4fc5567e2ed.d
@@ -1,7 +1,9 @@
-/home/matt/ICN/icn-blockchain/target/debug/deps/icn_blockchain-477cd4fc5567e2ed: src/main.rs src/blockchain.rs src/did.rs
+/home/matt/ICN/icn-blockchain/target/debug/deps/icn_blockchain-477cd4fc5567e2ed: src/main.rs src/blockchain.rs src/did.rs src/transaction.rs src/reputation.rs
 
-/home/matt/ICN/icn-blockchain/target/debug/deps/icn_blockchain-477cd4fc5567e2ed.d: src/main.rs src/blockchain.rs src/did.rs
+/home/matt/ICN/icn-blockchain/target/debug/deps/icn_blockchain-477cd4fc5567e2ed.d: src/main.rs src/blockchain.rs src/did.rs src/transaction.rs src/reputation.rs
 
 src/main.rs:
 src/blockchain.rs:
 src/did.rs:
+src/transaction.rs:
+src/reputation.rs:
